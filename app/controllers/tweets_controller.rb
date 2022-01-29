decb2951a5cf213a7511e205d91e16ecc9f57947
yuki-brave <93358518+yuki-brave@users.noreply.github.com>
2022-01-29 14:45:00 +0900

    tweet投稿の実装

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -7,5 +7,17 @@ class TweetsController < ApplicationController
   end
 
   def create
+    @tweet = Tweet.new(tweet_params)
+    if @tweet.save
+      redirect_to action: :index
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def tweet_params
+    params.require(:tweet).permit(:title, :text, :mind_id, :category_id, :time_zone_id).merge(user_id: current_user.id)
   end
 end
