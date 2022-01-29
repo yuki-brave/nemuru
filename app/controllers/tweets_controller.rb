e964ade58e3989996690e0ca55d5cd3ae973fe9f
yuki-brave <93358518+yuki-brave@users.noreply.github.com>
2022-01-29 19:38:40 +0900

    コントローラーの編集画面遷移の記述とページ遷移の条件分岐

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,8 @@
 class TweetsController < ApplicationController
-  before_action :authenticate_user!, only: [:new]
+  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :set_tweet, only: [:show, :edit, :update]
+  before_action :move_to_index, only: [:edit]
+ 
   def index
     @tweets = Tweet.includes(:user).order('created_at DESC')
   end
@@ -18,7 +21,17 @@ class TweetsController < ApplicationController
   end
 
   def show
-    @tweet = Tweet.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    if @tweet.update(tweet_params)
+      redirect_to action: :show
+    else
+      render :edit
+    end
   end
 
   private
@@ -26,4 +39,15 @@ class TweetsController < ApplicationController
   def tweet_params
     params.require(:tweet).permit(:title, :text, :mind_id, :category_id, :time_zone_id).merge(user_id: current_user.id)
   end
+
+  def set_tweet
+    @tweet = Tweet.find(params[:id])
+  end
+
+  def move_to_index
+    if current_user.id != @tweet.user_id
+      redirect_to action: :index
+    end
+  end
+
 end
