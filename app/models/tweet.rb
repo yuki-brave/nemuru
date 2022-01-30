f9535360aac8cc64f15f432e559abbc5286ccc70
yuki-brave <93358518+yuki-brave@users.noreply.github.com>
2022-01-30 11:50:15 +0900

    Rubocopによる修正

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -6,5 +6,5 @@ class Tweet < ApplicationRecord
   belongs_to :user
 
   validates :title, :text, presence: true
-  validates :mind_id, :category_id, :time_zone_id, numericality: { other_than: 1, message: "を入力してください" }
+  validates :mind_id, :category_id, :time_zone_id, numericality: { other_than: 1, message: 'を入力してください' }
 end
