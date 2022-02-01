6910fb881ac8215c5b0f2586e181f47b69dc3d23
yuki-brave <93358518+yuki-brave@users.noreply.github.com>
2022-02-01 16:13:24 +0900

    フォームオブジェクトにバリデーション記述

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -5,6 +5,4 @@ class Tweet < ApplicationRecord
   belongs_to :time_zone
   belongs_to :user
 
-  validates :title, :text, presence: true
-  validates :mind_id, :category_id, :time_zone_id, numericality: { other_than: 1, message: 'を選択してください' }
 end
--- app/models/tweet_tag.rb
+++ app/models/tweet_tag.rb
@@ -2,5 +2,8 @@ class TweetTag
   include ActiveModel::Model
   attr_accessor :title, :text, :mind_id, :category_id, :time_zone_id, :user_id
 
- 
+  with_option presence: true do
+    validates :title, :text, :user_id
+  end
+  validates :mind_id, :category_id, :time_zone_id, numericality: { other_than: 1, message: 'を選択してください' }
 end
