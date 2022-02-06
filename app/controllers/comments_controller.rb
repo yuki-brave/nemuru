12a2a58ff2c37390c0779cb4669f6414a73bee88
yuki-brave <93358518+yuki-brave@users.noreply.github.com>
2022-02-06 18:35:44 +0900

    Rubocopによる修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,14 +2,12 @@ class CommentsController < ApplicationController
   def create
     @comment = Comment.new(comment_params)
     @tweet = Tweet.find(params[:tweet_id])
-    if @comment.save
-      CommentChannel.broadcast_to @tweet, { comment: @comment, user: @comment.user }
-    end
+    CommentChannel.broadcast_to @tweet, { comment: @comment, user: @comment.user } if @comment.save
   end
 
   private
 
   def comment_params
-    params.require(:comment).permit(:text).merge(user_id:current_user.id, tweet_id: params[:tweet_id])
+    params.require(:comment).permit(:text).merge(user_id: current_user.id, tweet_id: params[:tweet_id])
   end
 end
--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :comment do
-    text { "MyText" }
+    text { 'MyText' }
     user_id { 1 }
     tweet_id { 1 }
   end
