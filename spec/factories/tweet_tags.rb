864f4527823b4e3150b62b8fef8a39e11ab54193
yuki-brave <93358518+yuki-brave@users.noreply.github.com>
2022-02-07 14:44:11 +0900

    カテゴリーの追加

--- spec/factories/tweet_tags.rb
+++ spec/factories/tweet_tags.rb
@@ -5,7 +5,7 @@ FactoryBot.define do
     thoughts     { Faker::Lorem.sentence }
     tag_name     { Faker::Lorem.word }
     mind_id      { Faker::Number.within(range: 1..3) }
-    category_id  { Faker::Number.within(range: 1..11) }
+    category_id  { Faker::Number.within(range: 1..13) }
     time_length_id { Faker::Number.within(range: 1..3) }
   end
 end
