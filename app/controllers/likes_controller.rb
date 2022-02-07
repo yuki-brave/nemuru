402bbc304bbe32c6aa50ae484161d591fcceae93
yuki-brave <93358518+yuki-brave@users.noreply.github.com>
2022-02-07 19:35:38 +0900

    いいね削除のコントローラーの定義

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -6,4 +6,12 @@ class LikesController < ApplicationController
       redirect_to tweet_path(params[:tweet_id])
     end
   end
+
+  def destroy
+    @like = Like.find_by(user_id: current_user.id, tweet_id: params[:tweet_id])
+    if @like.destroy
+      flash[:success] = "「いいね！」解除しました"
+      redirect_to tweet_path(params[:tweet_id])
+    end
+  end
 end
