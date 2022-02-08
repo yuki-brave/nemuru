ca27eed780207b3b661c3fe30df2fd42b9bc277b
yuki-brave <93358518+yuki-brave@users.noreply.github.com>
2022-02-08 22:52:12 +0900

    非同期のいいねのコントローラー定義

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,15 +1,20 @@
 class LikesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_tweet
+
   def create
-    @like = Like.new(user_id: current_user.id, tweet_id: params[:tweet_id])
-    if @like.save
-      redirect_to tweet_path(params[:tweet_id])
-    end
+    like = Like.new(user_id: current_user.id, tweet_id: params[:tweet_id])
+    like.save
   end
 
   def destroy
-    @like = Like.find_by(user_id: current_user.id, tweet_id: params[:tweet_id])
-    if @like.destroy
-      redirect_to tweet_path(params[:tweet_id])
-    end
+    like = Like.find_by(tweet_id: params[:tweet_id])
+    like.destroy
+  end
+
+  private
+
+  def set_tweet
+    @tweet = Tweet.find(params[:tweet_id])
   end
 end
