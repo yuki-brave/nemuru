e696a00797bce08809276229798ca413305b007c
yuki-brave <93358518+yuki-brave@users.noreply.github.com>
2022-02-08 14:38:05 +0900

    font-awesomeにていいねアイコン使用

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,8 +1,7 @@
 class LikesController < ApplicationController
   def create
-    @like = current_user.likes.new(tweet_id: params[:tweet_id])
+    @like = Like.new(user_id: current_user.id, tweet_id: params[:tweet_id])
     if @like.save
-      flash[:success] = "「いいね！」しました"
       redirect_to tweet_path(params[:tweet_id])
     end
   end
@@ -10,7 +9,6 @@ class LikesController < ApplicationController
   def destroy
     @like = Like.find_by(user_id: current_user.id, tweet_id: params[:tweet_id])
     if @like.destroy
-      flash[:success] = "「いいね！」解除しました"
       redirect_to tweet_path(params[:tweet_id])
     end
   end
