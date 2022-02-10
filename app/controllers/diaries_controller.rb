ded65275ad37cd3ea2edefbe45a469131bb9ee2a
yuki-brave <93358518+yuki-brave@users.noreply.github.com>
2022-02-10 20:56:13 +0900

    夢日記の投稿

--- app/controllers/diaries_controller.rb
+++ app/controllers/diaries_controller.rb
@@ -1,4 +1,24 @@
 class DiariesController < ApplicationController
   def index
   end
+
+  def new
+    @diary = Diary.new
+  end
+
+  def create
+    @diary = Diary.new(diary_params)
+    if @diary.save
+      redirect_to diaries_path
+    else
+      render :index
+    end
+  end
+
+    private
+
+    def diary_params
+      params.require(:diary).permit(:start_time, :title, :text, :thought, :mind_id, :category_id, :time_length_id).merge(user_id: current_user.id)
+    end
+  
 end
