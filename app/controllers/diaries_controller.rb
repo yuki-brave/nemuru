f077eefaa6a4d534bba44cdda37335ed494bffb5
yuki-brave <93358518+yuki-brave@users.noreply.github.com>
2022-02-11 01:19:18 +0900

    詳細画面の実装

--- app/controllers/diaries_controller.rb
+++ app/controllers/diaries_controller.rb
@@ -1,4 +1,6 @@
 class DiariesController < ApplicationController
+  before_action :set_diary, only: [:show]
+
   def index
     @diaries = Diary.includes(:user)
   end
@@ -16,10 +18,32 @@ class DiariesController < ApplicationController
     end
   end
 
+  def show
+    if @diary.mind_id.present?
+      @mind = Mind.find(@diary.mind_id)[:name]
+    else
+      @mind == nil
+    end
+    if @diary.category_id.present?
+      @category = Category.find(@diary.category_id)[:name]
+    else
+      @category == nil
+    end
+    if @diary.time_length_id.present?
+      @time_length = TimeLength.find(@diary.time_length_id)[:name]
+    else
+      @time_length == nil
+    end
+  end
+
   private
 
   def diary_params
     params.require(:diary).permit(:start_time, :title, :text, :thought, :mind_id, :category_id, :time_length_id).merge(user_id: current_user.id)
   end
   
+  def set_diary
+    @diary = Diary.find(params[:id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,5 @@ Rails.application.routes.draw do
     resources :likes, only: [:create, :destroy]
   end
   resources :users, only: :show
-  resources :diaries, only: [:index, :new, :create]
+  resources :diaries, only: [:index, :new, :create, :show]
 end
