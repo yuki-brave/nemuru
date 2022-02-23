425a03cfa931c2fbbc8fbe2222cc52e9a606be0a
yuki-brave <93358518+yuki-brave@users.noreply.github.com>
2022-02-23 19:03:25 +0900

    Rubocopによる修正

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -2,7 +2,7 @@ class TagsController < ApplicationController
   def index
     @tags = TweetTagRelation.where(tag_id: params[:id])
     @tag_name = Tag.find(params[:id]).tag_name
-    
+
     # @tag_tweets = @tag.includes(:user)
   end
 end
